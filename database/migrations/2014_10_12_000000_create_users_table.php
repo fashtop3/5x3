43427f82aa5544174d5d39c139770652667b2b23
Fashola Ayodeji <fashtop3@gmail.com>
2017-07-17 05:17:16 +0100

    setting up migration class

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,21 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->text('data')->nullable();
+            $table->integer('level_id')->nullable()->unsigned();
+            $table->foreign('level_id')
+                ->references('id')
+                ->on('levels');
+            $table->integer('upline_id')->nullable()->unsigned();
+            $table->foreign('upline_id')
+                ->references('id')
+                ->on('users');
+            $table->integer('level_assert')->unsigned();
+            $table->foreign('level_assert')
+                ->references('id')
+                ->on('levels');
+            $table->boolean('payment')->default(0);
+            $table->boolean('status')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2017_07_17_040648_create_receipts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReceiptsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('receipts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('upload');
+            $table->boolean('confirm')->default(0);
+            $table->integer('confirmed_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('receipts');
+    }
+}
