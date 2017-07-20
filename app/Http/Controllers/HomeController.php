3a5abe420c01f0ad114fadcbfe3f947b4d9c96a4
Femi Oladele <profemzy@gmail.com>
2017-07-20 21:56:30 +0100

    Playing with Dashboard Data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        if($user->upline_id){
+          $upline =  User::find($user->upline_id);
+            return view('home', compact(['user', 'upline']));
+        }
+        return view('home', compact('user'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,14 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    @if($user->payment == false)
+                        <p>Welcome to Glochis Platform</p>
+                        <a href="" class="btn btn-success">Activate Account</a>
+                    @elseif($upline)
+                        <p>Upline: {{$upline->name}}</p>
+                    @else
+
+                    @endif
                 </div>
             </div>
         </div>
