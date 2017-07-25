6d8996196495f217c02e7c378a0a50291fdb91b8
Fashola Ayodeji <fashtop3@gmail.com>
2017-07-25 15:58:39 +0100

    changed admin prefix

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -56,7 +56,7 @@ return [
     */
 
     // The prefix used in all base routes (the 'admin' in admin/dashboard)
-    'route_prefix' => 'admin',
+    'route_prefix' => '5x3admin',
 
     // Set this to false if you would like to use your own AuthController and PasswordController
     // (you then need to setup your auth routes manually in your routes.php file)
