cc31b846c99c91a36f4c90e3ba589945480cea64
Fashola Ayodeji <fashtop3@gmail.com>
2017-07-25 16:17:35 +0100

    extends backpack Role and Permission

---
+++ app/Permission.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Backpack\PermissionManager\app\Models\Permission as BPermission;
+
+class Permission extends BPermission
+{
+    //
+}
---
+++ app/Role.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Backpack\PermissionManager\app\Models\Role as BRole;
+
+class Role extends BRole
+{
+    //
+}
